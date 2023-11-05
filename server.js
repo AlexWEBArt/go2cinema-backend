4069195afdbb1b56c409ad05357b9a843164e5d8
AlexWebArt <elusiv@mail.ru>
2023-11-05 22:34:05 +0600

    added get route "/getlist"

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const cors = require('koa2-cors');
 const koaBody = require('koa-body');
-const mysql = require('koa-mysql')
 
 const mysql2 = require('mysql2/promise');
 
@@ -14,39 +13,36 @@ app.use(koaBody({
     json: true
 }));
 
-// Создаем подключение для выполнения SQL-скрипта
+const dbConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'go2cinemaDB',
+};
 
+const router = new Router();
 
-app.use(async (ctx) => {
+router.get('/getlist', async (ctx) => {
     try {
-        const connection = await mysql2.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'go2cinemaDB',
-        });
-        // Ваш SQL-скрипт для создания таблиц
-        const createTablesSQL = `
-            CREATE TABLE halls (
-                id INTEGER AUTO_INCREMENT PRIMARY KEY,
-                hall_name VARCHAR(255) NOT NULL,
-                hall_row VARCHAR(255) NOT NULL,
-                hall_places VARCHAR(255) NOT NULL
-            );
-        `;
+        const connection = await mysql2.createConnection(dbConfig);
 
-        // Выполняем SQL-скрипт для создания таблиц
-        await connection.query(createTablesSQL);
+        const [films] = await connection.execute('SELECT * FROM films');
+        const [halls] = await connection.execute('SELECT * FROM halls');
+        const [seances] = await connection.execute('SELECT * FROM seances');
+    
+        connection.end()
 
-        ctx.body = 'Tables created successfully';
+        ctx.body = { films, halls, seances };
     } catch (err) {
         ctx.status = 500;
         ctx.body = { error: err.message };
+        console.error('Error:', err);
     }
 });
 
-const port = process.env.PORT || 7071;
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = process.env.PORT || 7070;
 const server = http.createServer(app.callback());
 server.listen(port);
-console.log('Server listening on port ' + port);
 console.log('Server listening on port ' + port);
